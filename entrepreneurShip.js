74595ce38dba4c30a7f0499a2026a383ff7baabc
exomodal <exomodal@github.com>
2013-07-04 16:40:51 +0200

    initial

---
+++ entrepreneurShip.js
@@ -0,0 +1,21 @@
+if (Meteor.isClient) {
+  
+  /**
+   * Add linking for the navigation bar.
+   * These link to different Templates
+   */
+  Meteor.Router.add({
+    '/'           : 'start',
+    '/kaart'      : 'kaart',
+    '/brandstof'  : 'brandstof',
+    '/manifest'   : 'manifest',
+    '/hydroMeteo' : 'hydroMeteo'
+  });
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
